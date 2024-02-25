9b7b3cb88e285317a8896a49eb24624bde4518c6
Sing Ming Chen <sing.chen@bytedance.com>
2024-02-25 22:48:45 +0000

    ✨ Astro Island with Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://smmj.netlify.app/'
+  site: 'https://smmj.netlify.app/',
+  integrations: [preact()]
 });
